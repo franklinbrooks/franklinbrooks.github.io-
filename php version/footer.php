94d35f810ceee139bc02f76444c5b6ad220afaf3
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-02-19 12:14:49 -0500

    first commit

---
+++ php version/footer.php
@@ -0,0 +1,12 @@
+<footer class="relative">
+      <a href="mailto:fcb@franklinchristopherbrooks.com"><img src="images/mail-icon.png" alt="Email Icon" class="social-icon"></a>
+      <a href="https://twitter.com/franklincbrooks"><img src="images/twitter-icon.png" alt="Twitter Logo" class="social-icon"></a>
+      <a href="https://github.com/franklinchristopherbrooks"><img src="images/github-icon.png" alt="Github Logo" class="social-icon"></a>
+      <a href="https://www.linkedin.com/in/franklinchristopherbrooks/"><img src="images/linkedin-icon.png" alt="LinkedIn Logo" class="social-icon"></a>
+      <p>&copy; <script type="text/javascript">document.write(new Date().getFullYear());</script> Franklin Christopher Brooks. All rights reserved.</p>
+   <p>
+      <a href="http://validator.w3.org/check?uri=referer"><img
+          src="http://www.w3.org/Icons/valid-xhtml10-blue.png"
+          alt="Valid XHTML 1.0!" height="31" width="88" /></a>
+    </p>
+    </footer>
